2564a914432f2e4d47fe87799e231baf5cc30e94
patchwork-body <personal.gugfug@gmail.com>
2024-02-21 09:29:01 +0100

    feat: grpc call through graphql

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use async_graphql::{
     http::{playground_source, GraphQLPlaygroundConfig},
-    EmptyMutation, EmptySubscription, Schema,
+    Context, EmptyMutation, EmptySubscription, Schema,
 };
 use async_graphql_rocket::{GraphQLRequest, GraphQLResponse};
 use auth::auth_client::AuthClient;
@@ -16,8 +16,14 @@ pub(crate) struct QueryRoot;
 
 #[Object]
 impl QueryRoot {
-    async fn hello(&self) -> String {
-        "Hello GraphQL".to_owned()
+    async fn login(&self, ctx: &Context<'_>) -> String {
+        let mut grpc_client: AuthClient<Channel> =
+            ctx.data::<AuthClient<Channel>>().unwrap().clone();
+
+        let request = tonic::Request::new(auth::LoginRequest::default());
+        let response = grpc_client.login(request).await.unwrap().into_inner();
+
+        response.token
     }
 }
 
@@ -31,22 +37,22 @@ fn graphql_playground() -> rocket::response::content::RawHtml<String> {
 }
 
 #[rocket::post("/graphql", data = "<request>", format = "application/json")]
-async fn graphql(
-    schema: &rocket::State<SchemaType>,
-    _grpc_client: &rocket::State<AuthClient<Channel>>,
-    request: GraphQLRequest,
-) -> GraphQLResponse {
+async fn graphql(schema: &rocket::State<SchemaType>, request: GraphQLRequest) -> GraphQLResponse {
     request.execute(schema.inner()).await
 }
 
 #[rocket::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let grpc_client = AuthClient::connect("http://auth:50051").await?;
-    let schema = Schema::build(QueryRoot, EmptyMutation, EmptySubscription).finish();
+    let auth_service_ip =
+        std::env::var("AUTH_SERVICE_SERVICE_HOST").unwrap_or_else(|_| "127.0.0.1".to_string());
+
+    let grpc_client = AuthClient::connect(format!("http://{}:50051", auth_service_ip)).await?;
+    let schema = Schema::build(QueryRoot, EmptyMutation, EmptySubscription)
+        .data(grpc_client)
+        .finish();
 
     let _ = rocket::build()
         .manage(schema)
-        .manage(grpc_client)
         .mount("/", rocket::routes![graphql_playground])
         .mount("/v0", rocket::routes![graphql])
         .launch()
